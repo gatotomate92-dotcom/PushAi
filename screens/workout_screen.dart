e34fd1815d22d57339349083591d315127113de9
gatotomate92-dotcom <gatotomate92@gmail.com>
2026-07-30 23:04:38 +0300

    Update workout_screen.dart

--- screens/workout_screen.dart
+++ screens/workout_screen.dart
@@ -1,1 +1,43 @@
+import 'package:flutter/material.dart';
 
+class WorkoutScreen extends StatelessWidget {
+
+  const WorkoutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment:
+              MainAxisAlignment.center,
+
+          children: [
+
+            const Text(
+              "CAMERA MODE",
+              style: TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            const Text(
+              "Rep Count: 0",
+              style: TextStyle(
+                fontSize: 25,
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
