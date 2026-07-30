001d9f8a56817d535505ea1b7b3e1af50a405875
gatotomate92-dotcom <gatotomate92@gmail.com>
2026-07-30 22:56:33 +0300

    Update app.dart

--- app.dart
+++ app.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
 
+class PushAI extends StatelessWidget {
+
+  const PushAI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+
+      title: "PushAI",
+
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            "PUSH AI",
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
