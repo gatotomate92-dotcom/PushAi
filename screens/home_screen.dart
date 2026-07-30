2e21a10b1f81d63155efb1871fb76e364480287c
gatotomate92-dotcom <gatotomate92@gmail.com>
2026-07-30 22:57:52 +0300

    Update home_screen.dart

--- screens/home_screen.dart
+++ screens/home_screen.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      backgroundColor: Colors.black,
+
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment:
+              MainAxisAlignment.center,
+
+          children: [
+
+            const Text(
+              "PUSH AI",
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+
+            const SizedBox(height: 50),
+
+            ElevatedButton(
+
+              onPressed: () {},
+
+              child: const Text(
+                "START WORKOUT",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
